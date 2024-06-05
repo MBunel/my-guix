4da4c87ac12a902cc3073a9a7e5374ac88a4b97b
Mattia Bunel <mattia.bunel@ign.fr>
2024-06-05 12:16:16 +0200

    Correction of ressources packaging

--- packages/tyler.scm
+++ packages/tyler.scm
@@ -1,12 +1,13 @@
 (define-module (packages tyler)
   #:use-module (guix)
-  ;#:use-module (guix utils)
+  #:use-module (guix utils)
+  #:use-module (guix build utils)
   #:use-module (guix build-system cargo)
   #:use-module (guix git-download)
   #:use-module ((guix licenses)
                 #:prefix license:)
-  #:use-module (gnu packages)
   ;; Guix packages
+  #:use-module (gnu packages)
   #:use-module (gnu packages cpp)
   #:use-module (gnu packages geo)
   #:use-module (gnu packages curl)
@@ -17,12 +18,13 @@
   #:use-module (gnu packages sqlite)
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages llvm)
+  #:use-module (gnu packages python)
   ;; My-packages
   #:use-module (packages geoflow)
   #:use-module (packages geo)
   #:use-module (packages crates-io))
 
-;; Denine tyler package
+;; Define tyler package
 (define-public tyler
   (package
     (name "tyler")
@@ -38,7 +40,7 @@
        (patches (search-patches "packages/patches/tyler-config.patch"))
        (sha256
         (base32 "0x69ah00fk94bgrc0va9x35dzbyr9kdlqfxwfwd1f42n9qj19506"))))
-    (inputs (list sqlite proj-9.4 clang))
+    (inputs (list sqlite proj-9.4 clang python))
     (propagated-inputs (list geoflow))
     (native-inputs (list pkg-config cmake))
     (build-system cargo-build-system)
@@ -59,9 +61,24 @@
                        ("rust-subprocess" ,rust-subprocess-0.2)
                        ("rust-thiserror" ,rust-thiserror-1)
                        ("rust-walkdir" ,rust-walkdir-2))
-        #:tests? #f))
+       #:tests? #f
+       #:phases (modify-phases %standard-phases
+                  ;; Remove uses of "CARGO_MANIFEST_DIR" env var
+                  (add-after 'unpack 'patch-resources-folder
+                    (lambda* (#:key inputs #:allow-other-keys)
+                      (substitute* '("src/main.rs" "src/formats.rs"
+                                     "src/parser.rs")
+                        (("CARGO_MANIFEST_DIR")
+                         "out"))))
+                  ;; Copy resources in ./resources folder
+                  (add-after 'install 'copy-resources
+                    (lambda* (#:key inputs outputs #:allow-other-keys)
+                      (let* ((out (assoc-ref outputs "out"))
+                             (resources (string-append out "/resources")))
+                        (mkdir-p resources)
+                        (copy-recursively "resources" resources)))))))
     (home-page "https://github.com/3DGI/tyler")
-    (synopsis "Create tiles from 3D city objects encoded as CityJSONFeatures.")
+    (synopsis "Create tiles from 3D city objects encoded as CityJSONFeatures")
     (description
-     "Create tiles from 3D city objects encoded as @code{CityJSONFeatures}.")
+     "Create tiles from 3D city objects encoded as @code{CityJSONFeatures}")
     (license (list license:asl2.0))))
